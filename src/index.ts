6e1a9b25cd919b38c0e3a708aac3ae607a54ac72
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-04-11 14:24:50 +0200

    feat(db): init mongoDb && docker-compose

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import express from 'express';
 import indexRouter from '../routes/index.js';
+import mongoose from 'mongoose';
+
+
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+const mongoPort = process.env.MONGO_PORT || 27017;
+const mongoDb = process.env.MONGO_DB || 'my-database';
+
+const mongoUri = `mongodb://${mongoHost}:${mongoPort}/${mongoDb}`;
+const options: any = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(mongoUri, options)
+.then(() => {
+  console.log('Connected to database');
+})
+.catch((err) => console.log('Error connecting database', err.message));
+
 
 const app: express.Application = express();
 
